501ebde52951a730a73f6071b0102c57c31fb4da
Murali Krishna Mallela <98264698+muralimallela@users.noreply.github.com>
2022-07-25 18:00:00 +0530

    Class award of the student.
    Class award of the student based on his marks percentage.

---
+++ class_award_of_student.c
@@ -0,0 +1,24 @@
+/*
+Title   :  Class award of the student.
+Author  :  Murali Krishna.
+Date    : 25-07-2022.
+NOTE    : VERSION 1.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+  int marks;
+  char class[20];
+  printf("Enter your marks persentage: ");
+  scanf("%d",&marks);
+  if(marks>=70)
+    strcpy(class,"Distincion.");
+  else if(marks>=60)
+    strcpy(class,"First class.");
+  else if(marks>=40)
+    strcpy(class,"Second class.");
+  else
+    strcpy(class,"Fail.");
+  printf("\n\tThe result is: %s\n",class);
+}

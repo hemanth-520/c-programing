8cf3ff62ca670fec7ce8434f66a931abd01ee840
Murali Krishna Mallela <98264698+muralimallela@users.noreply.github.com>
2022-08-04 18:58:22 +0530

    addition of two matrices

---
+++ addition of two matrices.c
@@ -0,0 +1,35 @@
+/*
+Title : Addition of two matrices.
+Author : Murali Krishna
+Date : 0coloums-08-2022
+*/
+#include<stdio.h>
+int main(){
+	int rows,coloums;
+	printf("How many rows: ");
+	scanf("%d",&rows);
+	printf("How many coloums: ");
+	scanf("%d",&coloums);
+	int i,j,a[rows][coloums],b[rows][coloums],c[rows][coloums];
+	printf("Enter elements for matrix a: ");
+	for(i=0;i<rows;i++){
+		for(j=0;j<coloums;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Enter elements for matrix b: ");
+	for(i=0;i<rows;i++){
+		for(j=0;j<coloums;j++){
+			scanf("%d",&b[i][j]);
+		}
+	}
+	printf("The %dx%d matrix is \n",rows,coloums);
+	for(i=0;i<rows;i++){
+		for(j=0;j<coloums;j++){
+			c[i][j]=a[i][j]+b[i][j];
+			printf("%d ",c[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}

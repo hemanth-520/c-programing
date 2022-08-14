bd0e825ad0a041144c62a8cf96df67a8fa5a2c27
Murali Krishna Mallela <98264698+muralimallela@users.noreply.github.com>
2022-08-14 11:40:24 +0530

    Create multiplication_of_two_matrices.c

--- multiplication_of_two_matrices.c
+++ multiplication_of_two_matrices.c
@@ -6,39 +6,43 @@ Date: 06-08-2022
 #include<stdio.h>
 #define max 10
 int main(){
-    int rows1,col1,rows2,col2,i,j,k;
+    int rowsA,colA,rowsB,colB,i,j,k;
     int a[max][max],b[max][max], c[max][max];
     printf("Size of matrix A:\n");
     printf("How many rows? ");
-    scanf("%d",&rows1);
+    scanf("%d",&rowsA);
     printf("How many coloums? ");
-    scanf("%d",&col1);
+    scanf("%d",&colA);
     printf("Enter elements of marix A.\n");
-    for(i=0;i<rows1;i++){
-        for(j=0;j<col1;j++){
+    for(i=0;i<rowsA;i++){
+        for(j=0;j<colA;j++){
             scanf("%d",&a[i][j]);
         }
     }
     printf("\nSize of matrix B:\n");
     printf("How many rows? ");
-    scanf("%d",&rows2);
+    scanf("%d",&rowsB);
     printf("How many coloums? ");
-    scanf("%d",&col2);
+    scanf("%d",&colB);
     printf("Enter elements of marix B.\n");
-    for(i=0;i<rows2;i++){
-        for(j=0;j<col2;j++){
+    for(i=0;i<rowsB;i++){
+        for(j=0;j<colB;j++){
             scanf("%d",&b[i][j]);
         }
     }
-    printf("\nMultiplication of marix A and B is:\n");
-    for(i=0;i<rows1;i++){
-        for(j=0;j<col2;j++){
+    if(colA == rowsB){
+      printf("\nMultiplication of marix A and B is:\n");
+      for(i=0;i<rowsA;i++){
+        for(j=0;j<colB;j++){
             c[i][j]=0;
-            for(k=0;k<rows2;k++){
+            for(k=0;k<rowsB;k++){
                 c[i][j]+=a[i][k]*b[k][j];
             }
             printf("%d ",c[i][j]);
         }
         printf("\n");
+      }
     }
+    else
+      printf("\nDimension error.\n");
 }
